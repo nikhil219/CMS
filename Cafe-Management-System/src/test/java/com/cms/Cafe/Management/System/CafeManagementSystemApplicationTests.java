3355488f39f65e866966a2bd37e99c8e66f69eda
nikhil219 <46485539+nikhil219@users.noreply.github.com>
2023-06-17 22:53:31 +0530

    project Started

---
+++ Cafe-Management-System/src/test/java/com/cms/Cafe/Management/System/CafeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.Cafe.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
